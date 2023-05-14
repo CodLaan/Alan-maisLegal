4fe0dcaf8de070540f6b9b520c756c2220847119
Alan <128925482+CodLaan@users.noreply.github.com>
2023-05-14 19:12:10 -0300

    commit inicial
    oo

---
+++ script.js
@@ -0,0 +1,10 @@
+var yesButton = document.getElementById("yes");
+var noButton = document.getElementById("no");
+
+yesButton.addEventListener("click", function() {
+	window.location.href = "https://www.youtube.com/watch?v=xaPepCVepCg&ab_channel=BBCVIDEO_ID_SIM";
+});
+
+noButton.addEventListener("click", function() {
+	window.location.href = "https://www.youtube.com/watch?v=xaPepCVepCg&ab_channel=BBCVIDEO_ID_NAO";
+});
